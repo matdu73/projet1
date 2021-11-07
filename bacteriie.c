836ece62ae2e6d914a8a0e200cd1e0d791f0533e
matdu73 <maelle.regnier@epfl.ch>
2021-11-07 17:05:38 +0100

    voila mtnt ca print dans un fichier csv enfin faut que toi tu le fasse moi ca marche pas

--- bacteriie.c
+++ bacteriie.c
@@ -25,11 +25,18 @@ double concentration(int i, int x0, int y0, double c0){
 
 void gradc(double * tableau, struct position solute){
 	
+	
+	 FILE * file = fopen("gradientC.csv", "w");
 	int i = solute.y*100 + solute.x;
-	tableau[i]=solute.c;
 	for (int j=0; j<100*100; j++){
-		tableau[j]=concentration(j, solute.x, solute.y, solute.c);
-		printf("%f", tableau[j]);
+		if (j==i) tableau[i]=solute.c;
+		else tableau[j]=concentration(j, solute.x, solute.y, solute.c);
+		if (j%100==0) {
+			printf("\n");
+			fprintf(file, "\n");}
+		printf("%0.2f, ", tableau[j]);
+		fprintf(file, "%0.2f, ", tableau[j]);
+		
 }
 }
 
