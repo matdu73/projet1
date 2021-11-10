df631b446cb11ab0ada5b33954852678e4b3b585
matdu73 <mathilde.richard@epfl.ch>
2021-11-10 14:09:45 +0100

    maelle

--- bacteriie.c
+++ bacteriie.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-
+#include <time.h>
 
 struct position {
 	
@@ -21,61 +21,64 @@ double concentration(int i, int x0, int y0, double c0){
 	double c=c0/(sqrt(r));
 	return c;
 }
+void affichercapteur(struct position libelule) {
+    
+    printf(" x =  %d  y : %d\n ",libelule.x,libelule.y  );
 
+}
 
 void gradc(double * tableau, struct position solute){
 	
 	
-	 FILE * file = fopen("gradientC.csv", "w");
+	 FILE * gradient = fopen("gradientC.csv", "w");
 	int i = solute.y*100 + solute.x;
 	for (int j=0; j<100*100; j++){
 
 		tableau[j]=concentration(j, solute.x, solute.y, solute.c);
-		printf("%0.2f ", tableau[j]);
+	//	printf("%0.2f ", tableau[j]);
 
 		if (j==i) tableau[i]=solute.c;
 		else tableau[j]=concentration(j, solute.x, solute.y, solute.c);
 		if (j%100==0 & j!=0) {
 	//		printf("\n");
-			fprintf(file, "\n");}
-<<<<<<< HEAD
-			
-		printf("%0.2f ", tableau[j]);
-		fprintf(file, "%0.2f ", tableau[j]);
+			fprintf(gradient, "\n");}
+
+	//	printf("%0.2f ", tableau[j]);
+		fprintf(gradient, "%0.2f ", tableau[j]);
 //		fprintf(file, ",");
 
 
-}
-        
-}
 
 
-
-
-=======
-		printf("%0.2f, ", tableau[j]);
-		fprintf(file, "%0.2f, ", tableau[j]);
 		
 	}
 }
->>>>>>> 625946f47b9267fb9f124289082dcc703e02c699
+
 
 void deplacement(struct position robot, double * tableau, struct position solute){
-	
+	 FILE * trace = fopen("trace.csv", "w");
 	int i = robot.y*100 + robot.x;
 	int i0= solute.y*100 + solute.x;
-	double randomDomaine = RAND_MAX + 1.0;
-    int init = (rand() / randomDomaine * 3);
+	srand(time(NULL));
+    double randomDomaine = RAND_MAX + 1.0;
+
+ //   int init = (int) (rand() / randomDomaine * 3);
 	int dirx=1;
 	int diry=1;
-    
+    double	c_avant=tableau[i];
     while (i!= i0){
 		
-		if (diminution de concentration){
-			dirx =(rand() / randomDomaine * 3)-1;
-			diry =(rand() / randomDomaine * 3)-1;
+
+		if (tableau[i]<c_avant){
+
+
+			dirx =(int) (rand() / randomDomaine * 3)-1;
+			diry =(int) (rand() / randomDomaine * 3)-1;
 		}
-		
+
+	c_avant=tableau[i];
+		fprintf(trace, "   %d,   %d\n ",robot.x,robot.y);
+		affichercapteur(robot);
 		robot.x+=dirx;
 		robot.y+=diry;
 		i = robot.y*100 + robot.x;
@@ -89,12 +92,22 @@ void deplacement(struct position robot, double * tableau, struct position solute
 int main(int argc, char * argv[]) {
 
 	double * terrain = malloc(100*100*sizeof(double));
-	
+
 	struct position solute1 ={100,50,50};
 	struct position robot1={0,10,10};
+		gradc(terrain, solute1);
+	deplacement(robot1,terrain, solute1);
 	
-	//gradc(terrain, solute1);
+	
+	
+gradc(terrain, solute1);
+
+
 
 	free(terrain);
+	
+	
+	
 	return 0;
+	
 }
